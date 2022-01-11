4540dfa593c2601bd2f5fbe5d311deb052bb248e
Rohan Jinturkar <rjintu@users.noreply.github.com>
2022-01-10 21:30:04 -0500

    more changes

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # f21_iw
 Rohan's Independent Work in Natural Language Processing for Fall 2021
+
+
+Things to install:
+jq: https://stedolan.github.io/jq/download/ --> brew install jq
+
+Required: Python 3.8 or greater.
+
+Inside each JSON object:
+* We can get the author of the opinion (i.e. the judge who wrote)
+* We can get the actual text of the opinion
+
--- remove_duplicate_names.py
+++ remove_duplicate_names.py
@@ -1,56 +1,72 @@
-white_1 = ('Adam, Chip, Harry, Josh, Roger, Alan, Frank, Ian, Justin, Ryan, '
+WHITE_1 = ('Adam, Chip, Harry, Josh, Roger, Alan, Frank, Ian, Justin, Ryan, '
 'Andrew, Fred, Jack, Matthew, Stephen, Brad, Greg, Jed, Paul, Todd, Brandon, '
 'Hank, Jonathan, Peter, Wilbur, Amanda, Courtney, Heather, Melanie, Sara, '
 'Amber, Crystal, Katie, Meredith, Shannon, Betsy, Donna, Kristin, Nancy, Stephanie, '
 'Ellen, Lauren, Peggy, Colleen, Emily, Megan, Rachel, Wendy')
 
-black_1 = ('Alonzo, Jamal, Percell, Theo, Alphonse, Jerome, '
+BLACK_1 = ('Alonzo, Jamal, Percell, Theo, Alphonse, Jerome, '
 'Leroy, Torrance, Darnell, Lamar, Lionel, Rashaun, Tyree, Deion, Lamont, Malik, Terrence, Tyrone, Lavon, Marcellus, Terryl, Wardell, Aiesha, '
 'Shereen, Ebony, Latisha, Shaniqua, Teretha, Jasmine, Latonya, Tanisha, Tia, '
 'Lakisha, Latoya, Sharise, Yolanda, Malika, Tawanda, Yvette')
 
-white_2 =  ('Abigail, Allison, Amy, Anne, Bradley, Brett, Caitlin, Carly, Carrie, Claire, Cody, Cole, Colin, Connor, Dustin, Dylan, Emily, Emma, Garrett, '
+WHITE_2 =  ('Abigail, Allison, Amy, Anne, Bradley, Brett, Caitlin, Carly, Carrie, Claire, Cody, Cole, Colin, Connor, Dustin, Dylan, Emily, Emma, Garrett, '
 'Geoffrey, Greg, Hannah, Heather, Holly, Hunter, Jack, Jake, Jay, Jenna, Jill, '
 'Katelyn, Katherine, Kathryn, Katie, Kristen, Logan, Luke, Madeline, Matthew, '
 'Maxwell, Molly, Sarah, Scott, Tanner, Todd, Wyatt')
 
-black_2 = ('Aaliyah, Alexus, Darius, Darnell, DeAndre, DeShawn, Deja, Dominique, Ebony, Jada, Jamal, Jasmin, Jasmine, Jazmine, Jermaine, Keisha, Kiara, LaShawn, Latonya, Latoya, Precious, Rasheed, Raven, '
+BLACK_2 = ('Aaliyah, Alexus, Darius, Darnell, DeAndre, DeShawn, Deja, Dominique, Ebony, Jada, Jamal, Jasmin, Jasmine, Jazmine, Jermaine, Keisha, Kiara, LaShawn, Latonya, Latoya, Precious, Rasheed, Raven, '
 'Terrance, Tremayne, Tyrone, Xavier')
 
-white_3 = ('Molly, Amy, Claire, Emily, Katie, Madeline, Katelyn, Emma, '
+WHITE_3 = ('Molly, Amy, Claire, Emily, Katie, Madeline, Katelyn, Emma, '
 'Abigail, Carly, Jenna, Heather, Katherine, Caitlin, Kaitlin, Holly, Allison, Kaitlyn, Hannah, Kathryn, Jake, Connor, Tanner, Wyatt, Cody, Dustin, Luke, Jack, '
 'Scott, Logan, Cole, Lucas, Bradley, Jacob, Garrett, Dylan, Maxwell, Hunter, '
 'Brett, Colin')
 
-black_3 = ('Imani, Ebony, Aaliyah, Precious, Nia, Deja, Diamond, Asia, Jada, Tierra, Tiara, Kiara, Jazmine, Jasmin, Jazmin, Jasmine, Alexus, '
+BLACK_3 = ('Imani, Ebony, Aaliyah, Precious, Nia, Deja, Diamond, Asia, Jada, Tierra, Tiara, Kiara, Jazmine, Jasmin, Jazmin, Jasmine, Alexus, '
 'Raven, DeShawn, DeAndre, Marquis, Darnell, Terrell, Malik, Trevon, Tyrone, '
 'Willie, Dominique, Demetrius, Reginald, Jamal, Maurice, Jalen, Darius, Xavier, '
 'Terrance, Andre, Darryl')
 
-all_names = [white_1, black_1, white_2, black_2, white_3, black_3]
+# Extra sets, for testing
+BLACK_4 = ('Aisha, Ebony, Keisha, Kenya, Lakisha, Latonya, Latoya, Tamika, Tanisha, Darnell, Hakim, Jamal, Jermaine, Kareem, Leroy, Rasheed, Tremayne, Tyrone')
+
+BLACK_5 = ('Jalen DeAndre Malia Tanisha Jabari Taniya Darius Janae Hakim Kenya '
+           'Lamar Reginald Monique Latoya Keyshawn Divine Maurice Heaven Tremayne Lakisha '
+           'DaQuan Marques Shania Aisha Dwayne Keyana Jayvon Erykah Delroy Precious '
+           'Nia Cedric Jada Rasheed Marquise Bria Latrell Octavia Jerome Kiara '
+           'Ebony Aaliyah DeShawn Darnell Tamia Savion Tionna Denzel Keisha Kareem '
+           'Shanice Denisha Malcom Tyrone Kimani D’Andre Lyric Terrell Latonya Leroy '
+           'Tyra Jasmine Quincy Jamal Aniya Jaleel Kaylah Wendell Tamika Terell '
+           'Unique Chanel Andre Jermaine Amari Kevon Sade Tevin Ashanti Deshawn')
+
+all_names = [WHITE_1, BLACK_1, WHITE_2, BLACK_2, WHITE_3, BLACK_3, BLACK_4, BLACK_5]
 names_list = []
 
-for name_set in all_names:
+for name_set in all_names[:-1]:
     curr = name_set.split(',')
     for i in range(len(curr)):
         curr[i] = curr[i].strip()
     names_list.append(curr)
 
-all_white = set(names_list[0] + names_list[2] + names_list[4])
-all_black = set(names_list[1] + names_list[3] + names_list[5])
+curr = all_names[-1].split(' ')
+for i in range(len(curr)):
+    curr[i] = curr[i].strip()
+names_list.append(curr)
 
-print('Set of White names')
+all_white = set(names_list[0] + names_list[2] + names_list[4])
+all_black = set(names_list[1] + names_list[3] + names_list[5] + names_list[6] + names_list[7])
+
+print('Set of white names')
 print(all_white)
 print()
-print('Set of Black names')
+print('Set of black names')
 print(all_black)
 
 # pipe names to output file
-with open('white_names.txt', 'w') as white_output:
-    for name in all_white:
-        white_output.write(name + '\n')
+# with open('white_names.txt', 'w') as white_output:
+#     for name in all_white:
+#         white_output.write(name + '\n')
 
-with open('black_names.txt', 'w') as black_output:
+with open('black_names2.txt', 'w') as black_output:
     for name in all_black:
         black_output.write(name + '\n')
-
